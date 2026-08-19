a4992d07b5763497c922153f7a1a0bdc1a060db7
adibadm409 <adibadm409@gmail.com>
2026-08-19 03:37:13 +0300

    تعديل تلقائي: metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,41 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config')
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-config.resolver = {
-  ...config.resolver,
-  // expo-sqlite للويب يحمل محرك wa-sqlite كأصل WASM؛ يجب أن يبقى ضمن assetExts.
-  assetExts: Array.from(new Set([...(config.resolver.assetExts ?? []), 'wasm'])),
-  // منع Metro من مراقبة ملفات Android/iOS الأصلية الضخمة والمسارات غير اللازمة.
-  // نحتفظ بكل sourceExts وassetExts الافتراضية التي يضيفها Expo.
-  blockList: [
-    /node_modules\/react-native\/React(Android|Common|iOS)\/.*/,
-    /node_modules\/@react-native\/.+\/(android|ios|third-party-podspecs)\/.*/,
-    /node_modules\/expo\/.*\.(iml|gradle|pbxproj|c)$/,
-    /\.git\//,
-    /\/(android|ios)\//,
-  ],
-}
+config.watchFolders = [
+  path.resolve(__dirname, 'src'),
+];
 
-config.transformer = {
-  ...config.transformer,
-  getTransformOptions: async () => ({
-    transform: {
-      experimentalImportSupport: false,
-      inlineRequires: true,
-    },
-  }),
-}
-
-config.reporter = {
-  update(event) {
-    if (event.type === 'bundle_build_done') {
-      console.log('[METRO] Bundle ready (' + event.totalModules + ' modules)')
-    }
-    if (event.type === 'bundle_build_started') {
-      console.log('[METRO] Building bundle...')
-    }
-  },
-}
-
-module.exports = config
+module.exports = config;
