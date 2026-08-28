e13d782dbb46e49690a56c31b46224984ac6632f
adibadm409 <adibadm409@gmail.com>
2026-08-29 02:46:49 +0300

    تعديل تلقائي: metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,26 @@ config.watchFolders = [
   path.resolve(__dirname, 'src'),
 ];
 
+config.resolver.blockList = [
+  /\/python-agent\/.*/,
+  /\/audit\/.*/,
+  /\/docs\/.*/,
+  /\/memory\/.*/,
+];
+
+config.resolver.blacklistRE = [
+  /\/python-agent\/.*/,
+];
+
+if (config.resolver && Array.isArray(config.resolver.excludeNodeModules)) {
+  config.resolver.excludeNodeModules = [
+    /python-agent\/.*/,
+  ];
+}
+
+if (config.watcher) {
+  config.watcher.watchman = { deferStates: ['hg.update'] };
+  config.watcher.unstable_lazySha1 = true;
+}
+
 module.exports = config;
