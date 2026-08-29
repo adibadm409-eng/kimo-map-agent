b506d22de83f0bf6960ecc38a542c108c4d71014
adibadm409 <adibadm409@gmail.com>
2026-08-29 03:12:57 +0300

    تعديل تلقائي: TaskCard.tsx

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { useTheme } from '../theme/ThemeContext'
